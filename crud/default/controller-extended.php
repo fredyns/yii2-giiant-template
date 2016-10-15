1b2319e96afdb199fa5b8566fea509cbc4217e53
Fredy Nurman Saleh <email@fredyns.net>
2016-10-15 14:28:30 +0700

    add behavior

--- crud/default/controller-extended.php
+++ crud/default/controller-extended.php
@@ -7,10 +7,21 @@ echo "<?php\n";
 
 namespace <?= \yii\helpers\StringHelper::dirname(ltrim($generator->controllerClass, '\\')) ?>;
 
+use app\filters\Layout;
+
 /**
 * This is the class for controller "<?= $controllerClassName ?>".
 */
 class <?= $controllerClassName ?> extends <?= (isset($generator->controllerNs) ? '\\'.$generator->controllerNs.'\\' : '') .'base\\'.$controllerClassName."\n" ?>
 {
+    public function behaviors()
+    {
+        return [
+            'layout' => [
+                'class' => Layout::className(),
+            ],
+        ];
+    }
+
 
 }
